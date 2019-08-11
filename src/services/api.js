de07aff07d674e8df73f887e27e218f5561c0908
Hewerton Adão <37268237+Hewerton80@users.noreply.github.com>
2019-08-11 11:51:54 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_url = process.env.BASE_URL || 'http://localhost:3001'
+const base_url = process.env.REACT_APP_BASE_URL || 'http://localhost:3001'
 console.log('URL: '+ process.env.BASE_URL)
 
 const api = axios.create({
