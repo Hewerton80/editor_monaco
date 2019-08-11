d669d81fba0c8bc82924c94c8546de864e27b4e4
Hewerton Adão <37268237+Hewerton80@users.noreply.github.com>
2019-08-11 11:36:20 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 const api = axios.create({
 	baseURL: process.env.BASE_URL || 'http://localhost:3001'
+	console.log('URL: '+ process.env.BASE_URL)
 })
 export default api
